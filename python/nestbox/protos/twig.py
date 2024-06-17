691fd9d6086f6fb9089c9357c539291b5e9b2421
Dana Gretton <dana.gretton@gmail.com>
2024-06-17 14:26:18 -0400

    new measurement set class in python

--- python/nestbox/protos/twig.py
+++ python/nestbox/protos/twig.py
@@ -15,26 +15,58 @@ def covariance(measurement):
             idx += 1
     return cov
 
-class Twig:
-    def __init__(self, bytes):
-        self.twig = GeneratedTwig()
-        self.twig.ParseFromString(bytes)
-        if not self.twig.measurements or not self.twig.measurements[0].samples:
-            return
-        print(self.twig.measurements[0].samples[0])
-        print(self.twig.measurements[0].samples[0].mean)
-        print(self.twig.measurements[0].samples[0].covariance)
-        print(self.twig.measurements[0].samples[0].covariance.upper_triangle)
-        print(self.twig.measurements[0].transform)
 
-    def all_means(self):
-        return np.array([mean(sample) for measurement in self.twig.measurements for sample in measurement.samples])
-    
-    def covariance(self):
-        return np.array([covariance(measurement) for measurement in self.twig.measurements])
+class MeasurementSet:
+    def __init__(self, twig_ms):
+        self.dimensions = twig_ms.dimensions
+        self.is_homogeneous = twig_ms.isHomogeneous
+        self.means = []
+        self.covariances = []
+        for sample in twig_ms.samples:
+            self.means.append(mean(sample))
+            self.covariances.append(covariance(sample))
+        self.means = np.stack(self.means)
+        self.covariances = np.stack(self.covariances)
+        self.transform = np.array(twig_ms.transform)
 
     def __str__(self):
-        return str(self.twig)
+        return f"""MeasurementSet:
+        dimensions: {self.dimensions}
+        is_homogeneous: {self.is_homogeneous}
+        means: {self.means}
+        covariances: {self.covariances}
+        transform: {self.transform}"""
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Twig:
+    def __init__(self, bytes):
+        self.measurement_sets = []
+        self.gentwig = GeneratedTwig()
+        self.gentwig.ParseFromString(bytes)
+        self.stream_id = self.gentwig.streamId
+        self.coord_sys_id = self.gentwig.coordSysId
+        if not self.gentwig.measurements or not self.gentwig.measurements[0].samples:
+            return
+        print(self.gentwig.measurements[0].samples[0])
+        print(self.gentwig.measurements[0].samples[0].mean)
+        print(self.gentwig.measurements[0].samples[0].covariance)
+        print(self.gentwig.measurements[0].samples[0].covariance.upper_triangle)
+        print(self.gentwig.measurements[0].transform)
+        for ms in self.gentwig.measurements:
+            self.measurement_sets.append(MeasurementSet(ms))
+        print(self.measurement_sets[0])
+
+    def all_means(self):
+        return np.array([mean(sample) for measurement in self.gentwig.measurements for sample in measurement.samples])
+    
+    def covariance(self):
+        return np.array([covariance(measurement) for measurement in self.gentwig.measurements])
+    
+    def __str__(self):
+        return str(self.gentwig)
     
     def __repr__(self):
-        return str(self.twig)
+        return str(self.gentwig)
